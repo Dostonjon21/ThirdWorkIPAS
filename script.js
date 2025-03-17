d4086827bbcf9c82bfc2f67a9525144b4b094479
Doston12 <rajabovdost@gmail.com>
2025-03-17 15:13:00 +0500

    add

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 'use strict'
-const API="https://jsonplaceholder.typicode.com/users"
+const API = "https://jsonplaceholder.typicode.com/users"
 const container = document.querySelector('.container')
+
 fetch(API)
-.then((data)=>{
-    return data.json()
-}).then((data)=>{
-    console.log(data);
-
+.then((response) => response.json())
+.then((data) => {
+    container.innerHTML = ""; 
+    data.forEach((user) => {
+        container.innerHTML += `
+            <div class="user-card">
+                <img src="https://via.placeholder.com/150" alt="Profile Picture">
+                <h2>${user.name}</h2>
+                <p>Work: ${user.company.name}</p>
+                <a class="social" href="${user.website}" target="_blank">Visit Website</a>
+                <p>Email: ${user.email}</p>
+                <button class="btn">More info</button>
+            </div>
+        `;
+    });
 })
-
-
-
